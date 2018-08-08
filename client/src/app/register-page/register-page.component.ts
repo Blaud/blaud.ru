099f8b2f32a78b21e3a0d5c64d023c86539b8137
blaud <iblaudi@gmail.com>
2018-08-09 04:37:22 +0700

    basic client with auth

---
+++ client/src/app/register-page/register-page.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../shared/services/auth.service";
+import {ActivatedRoute, Params, Router} from "@angular/router";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Subscription} from "rxjs/index";
+
+@Component({
+  selector: 'app-register-page',
+  templateUrl: './register-page.component.html',
+  styleUrls: ['./register-page.component.css']
+})
+export class RegisterPageComponent implements OnInit {
+
+  form: FormGroup;
+  aSub: Subscription;
+
+  constructor(private auth: AuthService,
+              private router: Router,
+              private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+    });
+
+    this.route.queryParams.subscribe(
+      (params: Params) => {
+        if(params['registered']){
+          //use this email and pass to log in
+        } else if(params['accessDenied']) {
+          //log in before open this page
+        }
+      }
+    )
+  }
+
+  onSubmit() {
+    this.form.disable();
+    this.aSub = this.auth.register(this.form.value).subscribe(
+      () => this.router.navigate(['/login'], {
+        queryParams: {
+          registered: true
+        }
+      }),
+      error => {
+        console.warn(error);
+        this.form.enable()
+      }
+    )
+  }
+
+  ngOnDestroy(){
+    if(this.aSub){
+      this.aSub.unsubscribe()
+    }
+  }
+
+
+}

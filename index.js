644608f3214eea3e786d1528b22e95d11a75babc
blaud <iblaudi@gmail.com>
2018-08-10 14:57:29 +0700

    fix port

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const app = require('./app.js');
 const port = process.env.port || 3000;
 
 
-app.listen(3000, () =>console.log(`Server started on ${port}`));
+app.listen(port, () =>console.log(`Server started on ${port}`));

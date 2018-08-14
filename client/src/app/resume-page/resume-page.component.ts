141ec88a6a7a04eccf7f0231e05acfd6e7fe7c2e
blaud <iblaudi@gmail.com>
2018-08-14 18:20:15 +0700

    resume page component add to git ^^

---
+++ client/src/app/resume-page/resume-page.component.ts
@@ -0,0 +1,19 @@
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {MaterialService} from "../shared/classes/material.service";
+
+@Component({
+  selector: 'app-resume-page',
+  templateUrl: './resume-page.component.html',
+  styleUrls: ['./resume-page.component.css']
+})
+export class ResumePageComponent implements AfterViewInit {
+
+  @ViewChild('floating') floatingRef: ElementRef;
+
+  constructor() { }
+
+  ngAfterViewInit() {
+    MaterialService.initializeFloatingButton(this.floatingRef)
+  }
+
+}

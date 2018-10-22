3513fb41a5c3a5a46b98233eadccf58f884a0db3
blaud <iblaudi@gmail.com>
2018-10-23 00:21:50 +0700

    loaderIO verification fixed

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const path = require('path');
+const fs = require('fs');
 
 const keys = require('./config/keys');
 const authRoutes = require('./routes/auth');
@@ -35,6 +36,14 @@ app.use('/api/online_shop/analytics', analyticsRoutes);
 app.use('/api/online_shop/category', categoryRoutes);
 app.use('/api/online_shop/order', orderRoutes);
 app.use('/api/online_shop/position', positionRoutes);
+
+
+//create file for loader.io verification
+let stream = fs.createWriteStream('client/dist/client/'+keys.loaderio+'.txt');
+stream.once('open', function(fd) {
+    stream.write(keys.loaderio);
+    stream.end();
+});
 
 if (process.env.NODE_ENV === 'production'){
     app.use(express.static('client/dist/client'));
